61cee792e3dc606c55346ddd120bf3f816a44cc5
Djerian Noémie <91524499+noemiedjerian@users.noreply.github.com>
2022-01-12 23:54:22 +0100

    Update yaml.cpp

--- casaliShooter/yaml.cpp
+++ casaliShooter/yaml.cpp
@@ -1,10 +1,24 @@
+/**
+* @file yaml.cpp
+* @brief YAML : Allows to parse the configurable file 
+* @author Gonzales, Djerian, Leydier, Volpei, Dugourd
+* @version 1.0
+* @date 11/01/2022
+*/
 #include <vector>
+
 #include "fstream"
 
 #include "yaml.h"
 
 using namespace std;
 
+/** @brief Generator of a vector with ASCII references
+*
+*@param[in] fileName : Name of the file
+*@returns vector<unsigned> with ASCII references
+*
+*/
 vector<unsigned> vecParam (const string & fileName) {
     const vector<string> vecValuesUns = {"KBack", "KConfirm","KLeftMenu", "KRightMenu", "KMovingDownMenu","KMovingUpMenu", "KMovingLeftGame", "KMovingRightGame", "KShootGame", "KWidthMissile", "KLenghtMissile", "KWidthTorpedoPPs", "KLenghtTorpedoPPs", "KWidthTorpedoUFO", "KLenghtTorpedoUFO"};
     ifstream ifs(fileName);
